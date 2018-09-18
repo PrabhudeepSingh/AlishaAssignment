8ad5b8cccb037807efe86ff7366743b7ef5dba9d
prabhudeepsb <prabhudeepsb@gmail.com>
2018-09-18 16:48:04 +0530

    MyArrayList

--- AlishaAssignment/src/MyArrayList.java
+++ AlishaAssignment/src/MyArrayList.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
-public class MyArrayList {
-
+public class MyArrayList extends ArrayList<String>{
+	
+	public void display() {
+		for(int i=0; i<this.size(); i++ ) {
+			System.out.println(this.get(i));
+		}
+	}
+	
+	public void displayBackward() {
+		for(int i=this.size()-1; i>=0; i--) {
+			System.out.println(this.get(i));
+		}
+	}
+	
+	public void displayOddElements() {
+		for(int i=1; i<this.size(); i+=2) {
+			System.out.println(this.get(i));
+		}
+	}
 }
